934eec5a726aaa6d1a6e2700847b11f41898b4dc
iamadorl <92759913+iamadorl@users.noreply.github.com>
2021-12-01 12:12:26 +0100

    Add files via upload

---
+++ select/selectsignup.php
@@ -0,0 +1,14 @@
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>SIGNUP</title>
+		<link href="https://fonts.googleapis.com/css?family=Numans">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<a href="../logup/signup.php">Administrador</a>
+		<a href="../logup/signupstudent.php">Alumno</a>
+	</body>
+
+</html>
