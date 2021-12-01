054aeda415cb0c145f9be1fdb74dab6ff666cbc4
iamadorl <92759913+iamadorl@users.noreply.github.com>
2021-12-01 12:12:08 +0100

    Create selectlogin.php

---
+++ select/selectlogin.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>LOGIN</title>
+		<link href="https://fonts.googleapis.com/css?family=Numans">
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<a href="../logup/login.php">Administrador</a>
+		<a href="../logup/loginstudent.php">Alumno</a>
+	</body>
+
+</html>
