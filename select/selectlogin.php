58e4cfb8a086455925a9fde3fe51228655d58569
iamadorl <92759913+iamadorl@users.noreply.github.com>
2021-12-13 13:06:29 +0100

    Add files via upload

--- select/selectlogin.php
+++ select/selectlogin.php
@@ -3,11 +3,14 @@
 		<meta charset="utf-8">
 		<title>LOGIN</title>
 		<link href="https://fonts.googleapis.com/css?family=Numans">
-		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="../assets/css/style.css">
 	</head>
 	<body>
-		<a href="../logup/login.php">Administrador</a>
-		<a href="../logup/loginstudent.php">Alumno</a>
+		<?php require '../partials/header.php';?>
+		<h1>INICIO DE SESION DE USUARIOS</h1>
+		<h2>Selecciona el tipo de usuario</h2>	
+		<a  class="select" href="../logup/login.php">ADMINISTRADOR</a><br><br>
+		<a  class="select" href="../logup/loginstudent.php">ALUMNO</a>
 	</body>
 
 </html>
--- select/selectsignup.php
+++ select/selectsignup.php
@@ -1,14 +1,16 @@
-
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>SIGNUP</title>
 		<link href="https://fonts.googleapis.com/css?family=Numans">
-		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="../assets/css/style.css">
 	</head>
 	<body>
-		<a href="../logup/signup.php">Administrador</a>
-		<a href="../logup/signupstudent.php">Alumno</a>
+		<?php require '../partials/header.php';?>
+		<h1>REGISTRO DE USUARIOS</h1>
+		<h2>Selecciona el tipo de usuario</h2>	
+		<a class="select" href="../logup/signup.php">ADMINISTRADOR</a><br><br>
+		<a class="select" href="../logup/signupstudent.php">ALUMNO</a>
 	</body>
 
 </html>
